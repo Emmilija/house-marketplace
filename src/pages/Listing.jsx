7cb8bc06ada0d5bba81aa34f870bc22346c081da
Emilija Karatashesvka <karatashevskaemilija@gmail.com>
2022-08-26 13:55:27 +0200

    Swiper slider

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,19 +1,27 @@
 import {useState, useEffect} from 'react'
 import {Link, useNavigate, useParams} from 'react-router-dom'
 import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import {getDoc, doc} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 import {db} from '../firebase.config'
 import  Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
-import { async } from '@firebase/util'
+import 'swiper/css/bundle'
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+// import 'swiper/css/a11y';
+
 
 
 function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
-
+ 
   const navigate = useNavigate();
   const params = useParams();
   const auth = getAuth();
@@ -39,8 +47,27 @@ function Listing() {
 
   return (
     <main>
-      {/*Slider */}
-
+<Swiper
+    modules={[Navigation, Pagination, Scrollbar, A11y]}
+    slidesPerView={1}
+    pagination={{ clickable: true }}
+    navigation
+    style={{ height: '300px' }}
+>
+    {listing.imgUrls.map((url, index) => {
+       return (
+            <SwiperSlide key={index}>
+                <div
+                    className='swiperSlideDiv'
+                    style={{
+                        background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                        backgroundSize: 'cover',
+                    }}
+                ></div>
+            </SwiperSlide>
+        );
+    })}
+</Swiper>
       <div
         className="shareIconDiv"
         onClick={() => {
@@ -85,6 +112,8 @@ function Listing() {
             </li>
             <li>
             {listing.parking && 'Parking Spot'}
+            </li>
+            <li>
             {listing.furnished && 'Furnished'}
             </li>
         </ul>
